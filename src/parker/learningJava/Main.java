6042e23a556588b51b0cf280c246bae0a53d6c01
Christian Parker <christianparker512@gmail.com>
2021-09-07 14:03:54 -0500

    lisa and chris new accounts

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -3,6 +3,12 @@ package parker.learningJava;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Account chrissAccount = new Account ();
+	chrissAccount.withdrawal(100.0);
+	chrissAccount.deposit(50.0);
+	chrissAccount.deposit(51.25);
+
+    Account lisaAccount = new Account();
+    lisaAccount.deposit(100.00);
     }
 }
